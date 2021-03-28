dffd87a91a1779fb0f4352cdd6cbba36b98b26ae
Alessandro Gelmi <ale.gelmi2@gmail.com>
2021-03-28 18:09:21 +0200

    Analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React, {useEffect} from 'react'
 import "./App.scss";
 import CovidData from "./covid/daticovid";
+import ReactGa from 'react-ga';
 
 function App() {
+
+  useEffect(() => {
+    ReactGa.initialize('G-49SEEGFR24')
+    ReactGa.pageview('/')
+  })
+
   return (
     <div className="App">
         <CovidData />
